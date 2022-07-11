e38a072b0a78b28d7a0fc101a5bc7a8a756849f9
MinhNguyenDuc <nguyenducminh24101998@gmail.com>
2022-07-11 23:26:09 +0700

    Add Broker Service

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+const webPort = "8080"
+
+type Config struct{}
+
+func main() {
+
+	app := Config{}
+
+	log.Printf("Starting web server on port %v", webPort)
+
+	server := &http.Server{
+		Addr:    ":" + webPort,
+		Handler: app.routes(),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
---
+++ broker-service/cmd/api/routes.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	router.Use(middleware.Heartbeat("/ping"))
+	router.Use(middleware.Logger)
+	router.Get("/", app.Broker)
+
+	return router
+}
